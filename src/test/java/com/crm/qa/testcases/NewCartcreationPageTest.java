4044d319b59c5bc918b3290553d6f93191789519
Devendra.singh1 <Devendra.singh1@DevendraS-130398.FLYSPICE.COM>
2024-03-10 05:30:54 +0530

    cart and countray master posivite clow working fine

--- src/test/java/com/crm/qa/testcases/NewCartcreationPageTest.java
+++ src/test/java/com/crm/qa/testcases/NewCartcreationPageTest.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -121,6 +122,11 @@ public class NewCartcreationPageTest  extends TestBase {
              Thread.sleep(5000);
             }
          }
+	
+	@AfterMethod
+	public void tearDown(){
+		driver.quit();
+	}
 	}
 	
 
