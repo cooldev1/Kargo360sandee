0fe19e3df89aecedeb8bbbba0d0467346254bbc7
Devendra.singh1 <Devendra.singh1@DevendraS-130398.FLYSPICE.COM>
2024-03-10 21:18:53 +0530

    Cart_Country_Currancy and FreightForwarder Positive flow working fine 10 March 2024 9:20 pm

---
+++ src/test/java/com/crm/qa/testcases/FreightForwarder_MasterCurrencyMasterListingPageTest.java
@@ -0,0 +1,83 @@
+/*
+ * @author Naveen Khunteta
+ * 
+ */
+
+package com.crm.qa.testcases;
+
+import java.io.IOException;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.log4testng.Logger;
+
+import com.crm.qa.base.TestBase;
+import com.crm.qa.pages.CartMasterListingPage;
+import com.crm.qa.pages.HomePage;
+import com.crm.qa.pages.LoginPage;
+import com.crm.qa.util.TestUtil;
+
+public class FreightForwarder_MasterCurrencyMasterListingPageTest extends TestBase{
+
+	LoginPage loginPage;
+	HomePage homePage;
+	TestUtil testUtil;
+	CartMasterListingPage Cartlistingpage;
+	NewCartcreationPageTest NewCartCreationPage;
+	
+	com.crm.qa.pages.FreightForwarderMasterListingPage FreightForwarderMasterListingPage;
+	
+//	String sheetName = "contacts";
+	
+	   
+	public FreightForwarder_MasterCurrencyMasterListingPageTest(){
+			super();
+			
+	}
+	
+	
+	@BeforeMethod
+	public void setUp() throws InterruptedException {
+		
+		initialization();
+		testUtil = new TestUtil();
+		Cartlistingpage = new CartMasterListingPage();
+		loginPage = new LoginPage();
+		
+		FreightForwarderMasterListingPage = new com.crm.qa.pages.FreightForwarderMasterListingPage();
+		
+		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+	
+	}
+	
+	@Test(priority=1)
+	public void verifyhomepage() throws InterruptedException{
+		homePage.verifyHomePageurl();
+	}
+	
+	@Test(priority=2)
+	public void verifyFreightForwarderListingpage() throws InterruptedException{
+		homePage.verifyHomePageurl();
+		FreightForwarderMasterListingPage.clickOnFreightForwarderListingPage();
+		Thread.sleep(2000);
+		Assert.assertEquals(driver.getCurrentUrl(), "https://qaspicexpress.kargo360tech.com/freight-forwarder/list");
+	}
+		
+
+	@AfterMethod
+	public void tearDown(){
+		driver.quit();
+	}
+	
+	
+	
+	
+}
